a5925239e960247960b085d87ecda78a6fddd612
Amanda Walker <amanda@amandawalker.io>
2020-02-02 20:48:14 -0800

    Try a more visible comment color

--- dt-min-theme.el
+++ dt-min-theme.el
@@ -153,11 +153,11 @@
    `(warning                                   ((t (:foreground ,dt-min-bright_red :bold t))))
 
    ;; Built-in syntax
-   `(font-lock-builtin-face                    ((t (:foreground ,dt-min-light1))))
+   `(font-lock-builtin-face                    ((t (:foreground ,dt-min-light0))))
    `(font-lock-constant-face                   ((t (:foreground ,dt-min-sienna))))
-   `(font-lock-comment-face                    ((t (:foreground ,dt-min-light0_hard))))
+   `(font-lock-comment-face                    ((t (:foreground ,dt-min-neutral_cyan))))
    `(font-lock-function-name-face              ((t (:foreground ,dt-min-light4))))
-   `(font-lock-keyword-face                    ((t (:foreground ,dt-min-light1))))
+   `(font-lock-keyword-face                    ((t (:foreground ,dt-min-light0))))
    `(font-lock-string-face                     ((t (:foreground ,dt-min-sienna))))
    `(font-lock-variable-name-face              ((t (:foreground ,dt-min-aquamarine4))))
    `(font-lock-type-face                       ((t (:foreground ,dt-min-lightblue4))))
